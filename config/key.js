d282f17acaf77d9ea3df6ef7c9d32d9555ca0450
ogbeche77 <ogbeche77@yahoo.com>
2020-08-19 12:17:12 +0200

    Mongo Key hidden

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://ogbeche77:zidane77@kazeem-wx87j.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://ogbeche77:@kazeem-wx87j.mongodb.net/test?retryWrites=true&w=majority"
 };
